c1b41f6d54dddc1041d9b54fadc06d7de05315c6
Athos Martinez <athosandrade04@gmail.com>
2023-04-25 14:28:21 -0300

    .

--- Seção4_1/Seção4_1/Program.cs
+++ Seção4_1/Seção4_1/Program.cs
@@ -11,7 +11,7 @@ namespace MyApp // Note: actual namespace depends on the project name.
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Entre com os valores do triangulo x: ");
+            Console.WriteLine("Entre com os valores do triangulo X: ");
             Triangulo x, y;
             x = new Triangulo();
             y = new Triangulo();
@@ -19,7 +19,7 @@ namespace MyApp // Note: actual namespace depends on the project name.
             x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            Console.WriteLine("Digite os valores do trianuglo y: ");
+            Console.WriteLine("Digite os valores do trianuglo Y: ");
             y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
