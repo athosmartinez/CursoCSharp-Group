8ec04538bbf5b5910d98a24453ca24264b5d6f43
Athos Martinez <athosandrade04@gmail.com>
2023-04-25 14:26:34 -0300

    .

---
+++ Seção4_1/Seção4_1/Program.cs
@@ -0,0 +1,41 @@
+﻿using Seção4_1;
+using System;
+using System.ComponentModel;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Entre com os valores do triangulo x: ");
+            Triangulo x, y;
+            x = new Triangulo();
+            y = new Triangulo();
+
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Digite os valores do trianuglo y: ");
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double areaX = x.Area();
+            double areaY = y.Area();
+
+            Console.WriteLine("Area de X: " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Area de Y: " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+            if (areaX < areaY) { Console.WriteLine("A maior area é a do Y com " + areaY.ToString("F4", CultureInfo.InvariantCulture)); }
+            else if (areaX > areaY) { Console.WriteLine("A maior era é do X com " + areaX.ToString("F4", CultureInfo.InvariantCulture)); }
+            else { Console.WriteLine("São iguais!"); }
+        }
+
+
+
+    }
+}
---
+++ Seção4_1/Seção4_1/Triangulo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seção4_1
+{
+    internal class Triangulo
+    {
+        public double A;
+        public double B;
+        public double C;
+
+        public double Area()
+        {
+            double p = (A + B + C) / 2;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+        }
+
+    }
+
+
+
+}
