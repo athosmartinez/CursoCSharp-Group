417513b457f030762e822f376a581ac3ece34013
Athos Martinez <athosandrade04@gmail.com>
2023-04-26 10:00:26 -0300

    .

--- Seção5_1/Seção5_1/Program.cs
+++ Seção5_1/Seção5_1/Program.cs
@@ -1,39 +1,36 @@
-﻿using System.Globalization;
-namespace Course
+﻿using System;
+using System.Globalization;
+using Seção5_1;
+
+namespace MyApp // Note: actual namespace depends on the project name.
 {
-    class Produto
+    internal class Program
     {
-        public string Nome;
-        public double Preco;
-        public int Quantidade;
-        public double ValorTotalEmEstoque()
+        static void Main(string[] args)
         {
-            return Preco * Quantidade;
-        }
-        public void AdicionarProdutos(int quantidade)
-        {
-            Quantidade += quantidade;
-        }
-        public void RemoverProdutos(int quantidade)
-        {
-            Quantidade -= quantidade;
-        }
-        public override string ToString()
-        {
-            return Nome
-            + ", $ "
-            + Preco.ToString("F2", CultureInfo.InvariantCulture)
-            + ", "
-            + Quantidade
-            + " unidades, Total: $ "
-            + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+            Product p = new Product();
+            Console.WriteLine("Entre com os dados do produto:");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine());
+            Console.Write("Quantidade: ");
+            p.Quantidade = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int NewQntAdd = int.Parse(Console.ReadLine());
+            p.AddProduct(NewQntAdd);
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos que serão removidos: ");
+            int NewQntRemove = int.Parse(Console.ReadLine());
+            p.RemoveProduct(NewQntRemove);
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+
         }
     }
 }
-using System;
-using System.Globalization;
-namespace Course
-{
-    class Program
-    {
-        static void
