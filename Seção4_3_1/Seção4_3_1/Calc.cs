b43ef055eacef8cc5a85f01bbc7f268d8bfcd7a4
Athos Martinez <athosandrade04@gmail.com>
2023-04-26 09:30:54 -0300

    .

---
+++ Seção4_3_1/Seção4_3_1/Calc.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seção4_3_1
+{
+    internal class Calc
+    {
+        public double Pi = 3.14;
+        public double Circunferencia(double raio)
+        {
+            return (double)2 * Pi * raio;
+        }
+        public double Volume(double raio)
+        {
+            return (double)4 / 3 * Pi * Math.Pow(raio, 3);
+        }
+    }
+}
--- Seção4_3_1/Seção4_3_1/Program.cs
+++ Seção4_3_1/Seção4_3_1/Program.cs
@@ -5,7 +5,6 @@ namespace MyApp // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
-        static double Pi = 3.14;
         static void Main(string[] args)
         {
             Console.WriteLine("Entre o valor do raio: ");
@@ -16,18 +15,6 @@ namespace MyApp // Note: actual namespace depends on the project name.
             Console.WriteLine("Circunferencia: " + Circ.ToString("F2", CultureInfo.InvariantCulture));
             Console.WriteLine("Volume: " + Volum.ToString("F2", CultureInfo.InvariantCulture));
             Console.WriteLine("Valor de PI: " + Pi.ToString("F2", CultureInfo.InvariantCulture));
-
         }
-
-        static double Circunferencia(double raio)
-        {
-            return (double)2 * Pi * raio;
-        }
-        static double Volume(double raio)
-        {
-            return (double)4 / 3 * Pi * Math.Pow(raio, 3);
-        }
-
-
     }
 }
