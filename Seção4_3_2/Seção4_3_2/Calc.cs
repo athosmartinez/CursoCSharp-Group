e2faa0431d9b6c718758da2d5c3e20aa03a43e3e
Athos Martinez <athosandrade04@gmail.com>
2023-04-26 09:51:38 -0300

    .

---
+++ Seção4_3_2/Seção4_3_2/Calc.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seção4_3_2
+{
+    internal class Calc
+    {
+        public static double Pi = 3.14;
+        public static double Circunferencia(double raio)
+        {
+            return (double)2 * Pi * raio;
+        }
+        public static double Volume(double raio)
+        {
+            return (double)4 / 3 * Pi * Math.Pow(raio, 3);
+        }
+    }
+}
--- Seção4_3_2/Seção4_3_2/Program.cs
+++ Seção4_3_2/Seção4_3_2/Program.cs
@@ -1,4 +1,4 @@
-﻿using Seção4_3_1;
+﻿using Seção4_3_2;
 using System;
 using System.Globalization;
 
@@ -8,15 +8,15 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Calc calculadora = new Calc();
+            
             Console.WriteLine("Entre o valor do raio: ");
             double Raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            double Circ = calculadora.Circunferencia(Raio);
-            double Volum = calculadora.Volume(Raio);
+            double Circ = Calc.Circunferencia(Raio);
+            double Volum = Calc.Volume(Raio);
 
             Console.WriteLine("Circunferencia: " + Circ.ToString("F2", CultureInfo.InvariantCulture));
             Console.WriteLine("Volume: " + Volum.ToString("F2", CultureInfo.InvariantCulture));
-            Console.WriteLine("Valor de PI: " + calculadora.Pi.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de PI: " + Calc.Pi.ToString("F2", CultureInfo.InvariantCulture));
         }
     }
 }
