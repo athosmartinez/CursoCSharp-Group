4032c027a040c30198f6caf8d9eac01d4df1d1a4
Athos Martinez <athosandrade04@gmail.com>
2023-04-26 14:39:07 -0300

    .

---
+++ Seção5/Seção5_3/Seção5_3/Product.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seção5_3
+{
+    internal class Product
+    {
+        //atributo privado
+        private string _nome;
+        //propriedades autoimplementadas
+        public double Preco { get; private set; }
+        public int Quantidade { get; private set; }
+        //construtores
+        public Product() { }
+        public Product(string nome, double preco, int quantidade)
+        {
+            _nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+        //propriedades customizadas
+        public string Nome
+        {
+            get { return _nome; }
+            set
+            {
+                if (value != null && value.Length > 1)
+                {
+                    _nome = value;
+                }
+            }
+        }
+
+        //outros metodos
+        public double TotalValueStock()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void AddProduct(int quantidade)
+        {
+            if (quantidade > 0)
+            {
+                Quantidade = Quantidade + quantidade;
+
+            }
+        }
+
+        public void RemoveProduct(int quantidade)
+        {
+            if (quantidade > 0)
+            {
+                Quantidade = Quantidade - quantidade;
+
+            }
+        }
+
+        public override string ToString()
+        {
+            return _nome + ", $ " + Preco.ToString("F2", CultureInfo.InvariantCulture) + ", " + Quantidade + " unidades, Total: $" + TotalValueStock().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+
+    }
+}
---
+++ Seção5/Seção5_3/Seção5_3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Security.Principal;
+using Seção5_3;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Product produto = new Product("TV", 500, 10);
+            produto.Nome = "TV";
+
+
+            Console.WriteLine(produto.Nome);
+            Console.WriteLine(produto.Preco);
+            Console.WriteLine(produto.Quantidade);
+
+        }
+    }
+}
