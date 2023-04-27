ec9c4dad8a19f87096c428cf11e646ffeca94c43
Athos Martinez <athosandrade04@gmail.com>
2023-04-27 14:32:11 -0300

    .

---
+++ Seção6 -  Comportamento de mémoria, arrays e listas/Seção6_5/Seção6_5/Calculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seção6_5
+{
+    internal class Calculator
+    {
+        public static int Sum(params int[] nums)
+        {
+            int sum = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sum += nums[i];
+            }
+            return sum;
+        }
+
+    }
+}
---
+++ Seção6 -  Comportamento de mémoria, arrays e listas/Seção6_5/Seção6_5/Program.cs
@@ -0,0 +1,18 @@
+﻿using Seção6_5;
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int s1 = Calculator.Sum(2, 4);
+            int s2 = Calculator.Sum(4, 5, 10, 130, 4);
+            int s3 = Calculator.Sum(4, 5, 5, 5, 4);
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+            Console.WriteLine(s3);
+        }
+    }
+}
