ecb9a5ed9358c0bc1163a8acf712ee49e9b4a9cc
Athos Martinez <athosandrade04@gmail.com>
2023-04-27 09:25:05 -0300

    .

---
+++ Seção6 -  Comportamento de mémoria, arrays e listas/Seção6_2/Seção6_2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double? x = null;
+            double? y = 10;
+            Console.WriteLine(x.GetValueOrDefault());
+            Console.WriteLine(y.GetValueOrDefault());
+
+            if (x.HasValue)
+            {
+
+                Console.WriteLine(x.HasValue);
+            }
+            else
+            {
+                Console.WriteLine("X is null");
+
+            }
+            if (y.HasValue)
+            {
+                Console.WriteLine(y.Value);
+            }
+            else
+            {
+                Console.WriteLine("Y is null");
+
+            }
+
+
+            double? a = null;
+            double? b = 10;
+
+            double c = a ?? 5;
+            double d = b ?? 5;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
---
+++ Seção6 -  Comportamento de mémoria, arrays e listas/Seção6_3/Seção6_3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o numero de elementos do Array: ");
+            int n = int.Parse(Console.ReadLine());
+            double[] vect = new double[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Digite as alturas: ");
+                double altura = double.Parse(Console.ReadLine());
+                vect[i] = altura;
+            }
+
+        }
+    }
+}
