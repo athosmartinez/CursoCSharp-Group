52ce2552a66d292586e2ecdb7aab90fa1f7bfbba
Athos Martinez <athosandrade04@gmail.com>
2023-05-04 14:10:04 -0300

    .

---
+++ Seção10 - Herança e polimorfismo/Seção10_1/Seção10_1/Entities/BussinesAccount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seção10_1.Entities
+{
+    internal class BussinesAccount : Account
+    {
+        public double LoanLimit { get; set; }
+        public BussinesAccount() { }
+
+        public BussinesAccount(int number, string holder, double balance, double loanlimit) : base(number, holder, balance)
+        {
+            LoanLimit = loanlimit;
+        }
+
+        public void Loan(double amount)
+        {
+            if (amount <= LoanLimit)
+            {
+                Balance += amount;
+            }
+        }
+    }
+}
--- Seção10 - Herança e polimorfismo/Seção10_1/Seção10_1/Program.cs
+++ Seção10 - Herança e polimorfismo/Seção10_1/Seção10_1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Seção10_1.Entities;
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BussinesAccount account = new BussinesAccount(8100, "Bob", 100.00, 600);
+
+            Console.WriteLine(account.Balance);
+
+            
+        }
+    }
+}
