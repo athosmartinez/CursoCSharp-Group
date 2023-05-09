7e0eacedc0c6aaa14a03cc609c1485e4c6ab9070
Athos Martinez <athosandrade04@gmail.com>
2023-05-09 09:06:02 -0300

    .

---
+++ Seção10 - Herança e polimorfismo/Seção10_5/Seção10_5/Program.cs
@@ -0,0 +1,47 @@
+﻿using Seção10_5.Entities;
+using System;
+using System.Globalization;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employees = new List<Employee>();
+
+            Console.Write("Enter the number of employees: ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Employee #{i} data: ");
+                Console.Write("Outsourced (s/n)? ");
+                char ch = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Hours: ");
+                int hours = int.Parse(Console.ReadLine());
+                Console.Write("Value Per Hour: ");
+                double valuePerHour = double.Parse(Console.ReadLine());
+                if (ch == 'y')
+                {
+                    Console.Write("Additional charge: ");
+                    double addionalCharge = double.Parse(Console.ReadLine());
+                    employees.Add(new OutsourcedEmployee(name, hours, valuePerHour, addionalCharge));
+                }
+                else
+                {
+                    employees.Add(new Employee(name, hours, valuePerHour));
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("PAYMENTS: ");
+            foreach (Employee employee in employees)
+            {
+                Console.WriteLine(employee.Name + " - $ " + employee.Payment().ToString("F2", CultureInfo.InvariantCulture));
+
+
+            }
+        }
+    }
+}
