d63333b1ba2bae85e950cf1704622208da5569e3
Athos Martinez <athosandrade04@gmail.com>
2023-05-09 08:05:34 -0300

    .

---
+++ Seção10 - Herança e polimorfismo/Seção10_2/Seção10_2/Entities/SavingsAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seção10_2.Entities
+{
+    internal class SavingsAccount : Account
+    {
+        public double InteresRate { get; set; }
+
+        public SavingsAccount() { }
+
+        public SavingsAccount(int number, string holder, double balance, double interesRate) : base(number, holder, balance)
+        {
+            InteresRate = interesRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InteresRate;
+        }
+    }
+}
--- Seção10 - Herança e polimorfismo/Seção10_2/Seção10_2/Program.cs
+++ Seção10 - Herança e polimorfismo/Seção10_2/Seção10_2/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Seção10_2.Entities;
+using System;
 
 namespace MyApp // Note: actual namespace depends on the project name.
 {
@@ -6,7 +7,33 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Account acc = new Account(1001, "Alex,", 0.10);
+            BussinesAccount bacc = new BussinesAccount(1102, "Maria", 1000.00, 500000.00);
+
+            //UPCASTING
+            Account acc1 = bacc;
+            Account acc2 = new BussinesAccount(1001, "Pedro,", 300, 100);
+            Account acc3 = new SavingsAccount(1004, "Ana", 500, 0.01);
+
+            //DOWNCASTING
+
+            BussinesAccount acc4 = (BussinesAccount)acc2;
+            acc4.Loan(100.0);
+            //da errado - acc2.Loan(100.00) - pois acc2 é do tipo account, tem que fazer o downcasting
+            //BussinesAccount acc5 = (BussinesAccount)acc3;
+            if (acc3 is BussinesAccount)
+            {
+                BussinesAccount acc5 = (BussinesAccount)acc3;
+                acc5.Loan(200);
+                Console.WriteLine("Loan");
+            }
+            if(acc3 is SavingsAccount)
+            {
+                SavingsAccount acc5 = (SavingsAccount)acc3;
+                acc5.UpdateBalance();
+                Console.WriteLine("Update!");
+            }
+
         }
     }
 }
