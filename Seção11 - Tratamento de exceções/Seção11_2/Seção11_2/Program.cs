d4eca36f2a311d3d42e9c1be19d42769b4bb07de
Athos Martinez <athosandrade04@gmail.com>
2023-05-09 13:56:29 -0300

    .

---
+++ Seção11 - Tratamento de exceções/Seção11_2/Seção11_2/Program.cs
@@ -0,0 +1,31 @@
+﻿using Seção11_2.Entities;
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Room number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Checkin Date (dd/MM/yyyy): ");
+            DateTime checkIn = DateTime.Parse(Console.ReadLine());
+            Console.Write("Checkout Date (dd/MM/yyyy): ");
+            DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+
+            if (checkOut <= checkIn)
+            {
+                Console.WriteLine("Error in reservation: Checkout date must be after checkin");
+            }
+            else
+            {
+                Reservation reservation = new Reservation(number, checkIn, checkOut);
+                Console.WriteLine("Reservation: " + reservation);
+            }
+
+
+        }
+    }
+}
