f69e172e70b1694b269004e57ed9dc11bb103166
Athos Martinez <athosandrade04@gmail.com>
2023-05-09 13:22:41 -0300

    .

--- Seção11 - Tratamento de exceções/Seção11_1/Seção11_1/Program.cs
+++ Seção11 - Tratamento de exceções/Seção11_1/Seção11_1/Program.cs
@@ -6,7 +6,21 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+
+                int n1 = int.Parse(Console.ReadLine());
+                int n2 = int.Parse(Console.ReadLine());
+
+
+                int result = n1 / n2;
+
+                Console.WriteLine(result);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("ERROR!" + e.Message);
+            }
         }
     }
 }
