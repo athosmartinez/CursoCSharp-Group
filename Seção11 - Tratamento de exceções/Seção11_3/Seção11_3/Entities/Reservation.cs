695a9637c2117662b275faf18d1991acf2dccbf6
Athos Martinez <athosandrade04@gmail.com>
2023-05-10 07:23:02 -0300

    .

---
+++ Seção11 - Tratamento de exceções/Seção11_3/Seção11_3/Entities/Reservation.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seção11_3.Entities
+{
+    internal class Reservation
+    {
+
+        public int RoomNumber { get; set; }
+        public DateTime Checkin { get; set; }
+        public DateTime Checkout { get; set; }
+
+        public Reservation() { }
+
+        public Reservation(int roomNumber, DateTime checkin, DateTime checkout)
+        {
+            RoomNumber = roomNumber;
+            Checkin = checkin;
+            Checkout = checkout;
+        }
+
+        public int Duration()
+        {
+            TimeSpan duration = Checkout.Subtract(Checkin);
+            return (int)duration.TotalDays;
+        }
+
+        public string UpdateDates(DateTime checkIn, DateTime checkOut)
+        {
+            DateTime now = DateTime.Now;
+            if (checkIn < now || checkOut < now)
+            {
+                return " Reservation dates must be future dates";
+            }
+            if (checkOut <= checkIn)
+            {
+                return "Checkout date must be after checkin";
+            }
+
+            Checkin = checkIn;
+            Checkout = checkOut;
+            return null;
+
+        }
+
+        public override string ToString()
+        {
+            return "Room: " + RoomNumber + ", Checkin: " + Checkin.ToString("dd/MM/yyyy") + " Checkout: " + Checkout.ToString("dd/MM/yyyy") + ", " + Duration() + " nights.";
+        }
+    }
+}
---
+++ Seção11 - Tratamento de exceções/Seção11_3/Seção11_3/Program.cs
@@ -0,0 +1,52 @@
+﻿using Seção11_3.Entities;
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Room number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Checkin Date (dd/MM/yyyy): ");
+            DateTime checkIn = DateTime.Parse(Console.ReadLine());
+            Console.Write("Checkout Date (dd/MM/yyyy): ");
+            DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+
+            if (checkOut <= checkIn)
+            {
+                Console.WriteLine("Error in reservation: Checkout date must be after checkin");
+            }
+            else
+            {
+                Reservation reservation = new Reservation(number, checkIn, checkOut);
+                Console.WriteLine("Reservation: " + reservation);
+
+
+                Console.WriteLine();
+
+                Console.WriteLine("Enter data to update the reservation: ");
+                Console.Write("Checkin Date (dd/MM/yyyy): ");
+                checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Checkout Date (dd/MM/yyyy): ");
+                checkOut = DateTime.Parse(Console.ReadLine());
+                //Atualizar reserva
+
+                string error = reservation.UpdateDates(checkIn, checkOut);
+                if (error != null)
+                {
+                    Console.WriteLine("Error in reservation: " + error);
+                }
+                else
+                {
+                    Console.WriteLine("Reservetion: " + reservation);
+                }
+
+            }
+
+
+        }
+    }
+}
