f7d9eb5cbd8320f6fb882468e310a20fb3fe96b6
Athos Martinez <athosandrade04@gmail.com>
2023-05-10 08:12:08 -0300

    .

---
+++ Seção11 - Tratamento de exceções/Seção11_4/Seção11_4/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seção11_4.Entities.Exceptions
+{
+    internal class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message) { }
+    }
+}
