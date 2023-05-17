e68d5ac30d9c598d179123795a350051b96be22b
Athos Martinez <athosandrade04@gmail.com>
2023-05-17 12:09:08 -0300

    .

---
+++ Seção13 - Trabalhando com arquivos/Seção13_1/Seção13_1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string sourcePath = @"C:\Users\athos.andrade\source\repos\CursoCSharp-Group\Seção13 - Trabalhando com arquivos\Archives\file1.txt";
+            string targetPath = @"C:\Users\athos.andrade\source\repos\CursoCSharp-Group\Seção13 - Trabalhando com arquivos\Archives\file2.txt";
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath);
+                string[]lines = File.ReadAllLines(sourcePath);
+                foreach(string line in lines) {
+                    Console.WriteLine(line);
+                }
+
+
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("An error ocurred");
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
