aa23980d4482c64e3d2e26541e035e5a50392ad2
Athos Martinez <athosandrade04@gmail.com>
2023-05-23 07:43:55 -0300

    .

---
+++ Seção14 - Interfaces/Seção14_2 - Com interface/Seção14_2/Services/BrasilTaxService.cs
@@ -0,0 +1,24 @@
+﻿using Seção14_2.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seção14_1.Services
+{
+    internal class BrasilTaxService : ITaxService
+    {
+        public double Tax(double amonunt)
+        {
+            if (amonunt <= 100)
+            {
+                return amonunt * 0.2;
+            }
+            else
+            {
+                return amonunt * 0.15;
+            }
+        }
+    }
+}
---
+++ Seção14 - Interfaces/Seção14_2 - Com interface/Seção14_2/Services/ITaxService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seção14_2.Services
+{
+    internal interface ITaxService
+    {
+        double Tax(double amount);
+    }
+}
