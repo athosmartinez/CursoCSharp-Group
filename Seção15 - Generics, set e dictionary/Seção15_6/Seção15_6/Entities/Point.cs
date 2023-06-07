d930eb87566b4d47cefa755905d7ff7be3ed9394
Athos Martinez <athosandrade04@gmail.com>
2023-06-07 10:43:58 -0300

    .

---
+++ Seção15 - Generics, set e dictionary/Seção15_6/Seção15_6/Entities/Point.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seção15_6.Entities
+{
+    internal class Point
+    {
+
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Point(int x, int y) : this()
+        {
+            X = x;
+            Y = y;
+
+        }
+}
