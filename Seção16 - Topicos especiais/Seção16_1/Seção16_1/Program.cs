6c49385fc10a4561345662c5d9a86bfa5faffe6c
Athos Martinez <athosandrade04@gmail.com>
2023-06-07 13:59:29 -0300

    .

---
+++ Seção16 - Topicos especiais/Seção16_1/Seção16_1/Program.cs
@@ -0,0 +1,15 @@
+﻿using Seção16_1.Extensios;
+using System;
+
+namespace Seção16_1 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2018, 11, 16, 8, 10, 45);
+            Console.WriteLine(dt.ElapsedTime());
+
+        }
+    }
+}
---
+++ Seção16 - Topicos especiais/Seção16_2/Seção16_2/Program.cs
@@ -0,0 +1,15 @@
+﻿using Seção16_2.Extensios;
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String s1 = "Good morning dear students!";
+            Console.WriteLine(s1.Cut(10));
+           
+        }
+    }
+}
